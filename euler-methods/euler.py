ce7b40e7b6dc7d36d8fc50b009f3187a55cb5205
Lucio Bueno Vieira Junior <luciobvjr@gmail.com>
2022-07-21 19:02:15 -0300

    instal scipy and add other euler methods

--- euler-methods/euler.py
+++ euler-methods/euler.py
@@ -1,24 +1,24 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-def F(x,y): #y'
-    f = y + np.cos(y) - x 
+def F(y): #y'
+    f = y**2
     return f
 
 def Euler_explicito():
     h = float(input('Valor de h = '))
     xbar = float(input('Valor de x = '))
-    n = (xbar - 1) / h 
+    n = xbar / h 
     n = int(n)
     x = np.zeros(n+1)
     y = np.zeros(n+1)
-    x[0] = 1
-    y[0] = 2
+    x[0] = 0
+    y[0] = 3
 
     for i in np.arange(0,n):
-        y[i+1] = y[i] + h * F(x[i],y[i])
+        y[i+1] = y[i] + h * F(y[i])
         x[i+1] = x[i] + h
-
+    
     print('O valor de y correspondente =',y[-1])
     plt.plot(x,y)
     plt.show()
